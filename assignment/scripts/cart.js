5a8fa5ee2f4293efafafc25a9dd30f847c525015
Jade Wiegel <jadewiegel@gmail.com>
2022-10-25 09:36:00 -0400

    completed main assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,20 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+const basket = [];
+
+function addItem(item) {
+    basket.push(item);
+} //end addItem
+
+addItem('butter');
+addItem('milk');
+console.log('Added item to list', basket);
+
+function itemsList() {
+    for (let i = 0; i < basket.length; i++) {
+    console.log(basket [i]);
+    }
+}
+itemsList();
